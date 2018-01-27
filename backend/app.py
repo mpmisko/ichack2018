240dcd2f77d8f35226b40d95374cd496f8e5c320
mp5217 <michal.pandy17@imperial.ac.uk>
2018-01-27 13:17:27 +0000

    stub json

--- backend/app.py
+++ backend/app.py
@@ -8,16 +8,30 @@ from threading import Thread
 
 app = Flask(__name__, static_url_path='')
 frontend_test_json = {
-
+  "name": "batman",
+  "picutre": "https://goo.gl/kqBGik",
+  "avg_color": "#FF0000",
+  "emotions": {
+    "anger": 0.15,
+    "contempt": 0.05,
+    "disgust": 0.1,
+    "fear": 0.04,
+    "happiness": 0.4,
+    "neutral": 0.23,
+    "sadness": 0.1,
+    "surprise": 0.02
+  },
+  "words": ["war", "fast", "mjolnir", "earth", "conquer"],
+  "objects": ["batmobile", "clown", "car", "gun", "villain"]
 }
 
 @app.route('/frontend_test')
 def index():
-    return "Nothing here :)"
+    return frontend_test_json
 
 @app.route('/process')
 def process():
-    return "Some json"
+    return "not ready yet"
 
 
 if __name__ == '__main__':
